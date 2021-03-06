1f4d29a2782a1bbbade294bf1724b7396d5ba4c0
Prophethor <prophethor@gmail.com>
2021-03-06 23:59:38 +0100

    Core game loop implemented
    Player can't move more than 2 tiles par tap, lowers his hit chance for each tile over 3, shoot phase has a minigame to power up hit chance, when minigames ends random generator decides if shots hit and then everything resets untill player or enemy dies

--- H1ghN00n/Assets/Scripts/GameManager.cs
+++ H1ghN00n/Assets/Scripts/GameManager.cs
@@ -1,61 +1,196 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     GridGenerator gg;
-    public enum Direction { UL, UR, L, C, R, DL, DR };
-    public static Dictionary<Direction, Vector2Int> HexDir = new Dictionary<Direction, Vector2Int>(){
-        { Direction.UL, new Vector2Int(-1,1) },
-        { Direction.UR, new Vector2Int(0,1) },
-        { Direction.L, new Vector2Int(-1,0) },
-        { Direction.C, new Vector2Int(0,0) },
-        { Direction.R, new Vector2Int(1,0) },
-        { Direction.DL, new Vector2Int(0,-1) },
-        { Direction.DR, new Vector2Int(1,-1) }
-    };
-
-    Vector2Int playerPosCoord, enemyPosCoord;
-    Vector3 playerPos, enemyPos, playerDest, enemyDest;
+    
+    int width = 8, height = 5;
+    int turns = 3;
+    float minigameTime = 0;
+    float currentTime = 0;
+    Vector2Int playerCoord, enemyCoord;
+    Vector3 playerDest, enemyDest;
     public GameObject playerPrefab, enemyPrefab;
+    public Text playerChanceText, playerHpText, enemyChanceText, enemyHpText, timerText;
     GameObject player, enemy;
     private Vector3 playerOffset = new Vector3(0,0.3f,-1);
-    private float moveSpeed = 5;
+    private float moveSpeed = 8;
+    int playerChance = 50, enemyChance = 50, playerHp = 3, enemyHp = 3;
 
     private void Start() {
 
         gg = GameObject.FindObjectOfType<GridGenerator>();
-        gg.GenerateGrid(new Vector2Int(8,5));
+        gg.GenerateGrid(new Vector2Int(width, height));
 
-        playerPosCoord = new Vector2Int(3, 2);
-        playerPos = gg.GetWorldPos(playerPosCoord);
-        enemyPosCoord = new Vector2Int(4, 2);
-        enemyPos = gg.GetWorldPos(enemyPosCoord);
-        player = Instantiate(playerPrefab, playerPos, Quaternion.identity);
-        enemy = Instantiate(enemyPrefab, enemyPos, Quaternion.identity);
-        playerDest = player.transform.position;
-        enemyDest = enemy.transform.position;
+        playerCoord = new Vector2Int(3, 2);
+        enemyCoord = new Vector2Int(4, 2);
+        player = Instantiate(playerPrefab, gg.GetWorldPos(playerCoord) + playerOffset, Quaternion.identity);
+        gg.hexGrid[playerCoord].SetState(GridGenerator.HexState.Player);
+        enemy = Instantiate(enemyPrefab, gg.GetWorldPos(enemyCoord) + playerOffset, Quaternion.identity);
+        gg.hexGrid[enemyCoord].SetState(GridGenerator.HexState.Enemy);
+        playerDest = player.transform.position - playerOffset;
+        enemyDest = enemy.transform.position - playerOffset;
+
+        StartCoroutine(Move());
     }
-
 
     private void Update() {
 
-        player.transform.position = Vector3.MoveTowards(player.transform.position, playerDest+playerOffset, moveSpeed * Time.deltaTime);
-        enemy.transform.position = Vector3.MoveTowards(enemy.transform.position, enemyDest+playerOffset, moveSpeed * Time.deltaTime);
+        playerChanceText.text = playerChance.ToString();
+        playerHpText.text = playerHp.ToString();
+        enemyChanceText.text = enemyChance.ToString();
+        enemyHpText.text = enemyHp.ToString();
+        timerText.text = (Mathf.Min((int)(10 - currentTime)+1,10)).ToString();
+        
+    }
 
-        if (Vector3.Distance(player.transform.position, playerDest + playerOffset) < .05f ||
-            Vector3.Distance(enemy.transform.position, enemyDest + playerOffset) < .05f) {
-            if (Input.GetMouseButtonDown(0)) {
-                RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), -Vector2.up);
-                if (hit.collider != null) {
-                    playerDest = hit.transform.position;
-                    enemyDest = gg.hexGrid[enemyPosCoord].transform.position + gg.hexGrid[playerPosCoord].transform.position - playerDest;
+    IEnumerator Move() {
+        Debug.Log("You have 10 seconds!");
+        while (true) {
+            yield return null;
+            currentTime += Time.deltaTime;
+            player.transform.position = Vector3.MoveTowards(player.transform.position, playerDest + playerOffset, moveSpeed * Time.deltaTime);
+            enemy.transform.position = Vector3.MoveTowards(enemy.transform.position, enemyDest + playerOffset, moveSpeed * Time.deltaTime);
+            if (Vector3.Distance(player.transform.position, playerDest + playerOffset) < .05f) {
+                if (currentTime > 10f) {
+                    Debug.Log("Time run out!");
+                    yield return StartCoroutine(Shoot());
+                    break;
+                } else if (Input.GetKeyDown(KeyCode.Space)) {
+                    yield return StartCoroutine(Shoot());
+                    break;
+                } else {
+                    if (Input.GetMouseButtonDown(0)) {
+                        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), -Vector2.up);
+                        if (hit.collider != null) {
+                            int dist = gg.GetDistance(gg.hexGrid[playerCoord].GetTile(), hit.transform.gameObject);
+                            if(dist <= 2) {
+                                playerDest = hit.transform.position;
+                                Vector2Int coordDif = gg.GetCoord(hit.transform.gameObject) - playerCoord;
+                                gg.hexGrid[playerCoord].SetState(GridGenerator.HexState.Empty);
+                                playerCoord = gg.GetCoord(hit.transform.gameObject);
+                                gg.hexGrid[playerCoord].SetState(GridGenerator.HexState.Player);
+                                gg.hexGrid[enemyCoord].SetState(GridGenerator.HexState.Empty);
+                                enemyCoord = enemyCoord - coordDif;
+                                gg.hexGrid[enemyCoord].SetState(GridGenerator.HexState.Enemy);
+                                enemyDest = gg.hexGrid[enemyCoord].GetTile().transform.position + gg.hexGrid[playerCoord].GetTile().transform.position - playerDest;
+                                turns -= dist;
+                                if (turns < 0) playerChance = Mathf.Max(playerChance - 10 * dist, 0);
+                            }
+                        }
+                    }
                 }
             }
         }
-        
+    }
 
+    IEnumerator Shoot() {
+        Debug.Log("Shoot!");
+        foreach(GridGenerator.Hex hex in gg.hexGrid.Values) {
+            if (hex.GetState() == GridGenerator.HexState.Empty) {
+                hex.GetTile().GetComponent<SpriteRenderer>().color = Color.gray;
+            }
+        }
+        
+        List<Vector2Int> tiles = new List<Vector2Int>(gg.hexGrid.Keys);
+        while (true) {
+
+            yield return StartCoroutine(Minigame());
+
+            int playerShot = Random.Range(0, 100);
+            if (playerShot < playerChance) {
+                Debug.Log("Player shot the enemy");
+                enemyHp -= 1;
+            } else Debug.Log("Player missed");
+            int enemyShot = Random.Range(0, 100);
+            if (enemyShot < enemyChance) {
+                Debug.Log("Enemy shot the player");
+                playerHp -= 1;
+            } else Debug.Log("Enemy missed");
+
+            if (playerHp <= 0) {
+                Debug.Log("Player lost!");
+                break;
+            } else if (enemyHp <= 0) {
+                Debug.Log("Player won!");
+                break;
+            } else {
+                Debug.Log("New round!");
+                StartCoroutine(Reset());
+                break;
+            }
+        }
+    }
+
+    IEnumerator Minigame() {
+        minigameTime = 0;
+        while (minigameTime < 3) {
+            yield return StartCoroutine(TapTile());
+        }
+    }
+
+    IEnumerator TapTile() {
+        List<Vector2Int> tiles = new List<Vector2Int>(gg.hexGrid.Keys);
+        GridGenerator.Hex activeTile = gg.hexGrid[tiles[Random.Range(0, tiles.Count)]];
+        while(activeTile.GetState()!=GridGenerator.HexState.Empty) activeTile = gg.hexGrid[tiles[Random.Range(0, tiles.Count)]];
+        activeTile.GetTile().GetComponent<SpriteRenderer>().color = Color.white;
+        activeTile.SetState(GridGenerator.HexState.Active);
+        while (true) {
+
+            yield return null;
+            minigameTime += Time.deltaTime;
+            if (minigameTime > 3) break;
+
+            if (Input.GetMouseButtonDown(0)) {
+                RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), -Vector2.up);
+                if (hit.collider != null && hit.transform.gameObject == activeTile.GetTile()) {
+                    activeTile.GetTile().GetComponent<SpriteRenderer>().color = Color.grey;
+                    activeTile.SetState(GridGenerator.HexState.Empty);
+                    activeTile = gg.hexGrid[tiles[Random.Range(0, tiles.Count)]];
+                    while (activeTile.GetState() != GridGenerator.HexState.Empty) activeTile = gg.hexGrid[tiles[Random.Range(0, tiles.Count)]];
+                    activeTile.GetTile().GetComponent<SpriteRenderer>().color = Color.white;
+                    activeTile.SetState(GridGenerator.HexState.Active);
+                    playerChance += Random.Range(3,7);
+                }
+            }
+        }
+    }
+
+    IEnumerator Reset() {
+
+        foreach (GridGenerator.Hex hex in gg.hexGrid.Values) {
+            if (hex.GetState() == GridGenerator.HexState.Active) {
+                hex.SetState(GridGenerator.HexState.Empty);
+            } else if (hex.GetState() == GridGenerator.HexState.Empty) {
+                hex.GetTile().GetComponent<SpriteRenderer>().color = Color.white;
+            }
+        }
+
+        gg.hexGrid[playerCoord].SetState(GridGenerator.HexState.Empty);
+        playerCoord = new Vector2Int(3, 2);
+        gg.hexGrid[playerCoord].SetState(GridGenerator.HexState.Player);
+        playerDest = gg.GetWorldPos(playerCoord);
+
+        gg.hexGrid[enemyCoord].SetState(GridGenerator.HexState.Empty);
+        enemyCoord = new Vector2Int(4, 2);
+        gg.hexGrid[enemyCoord].SetState(GridGenerator.HexState.Enemy);
+        enemyDest = gg.GetWorldPos(enemyCoord);
+        while (Vector3.Distance(player.transform.position, playerDest + playerOffset) > .05f) {
+            yield return null;
+            player.transform.position = Vector3.MoveTowards(player.transform.position, playerDest + playerOffset, moveSpeed * Time.deltaTime);
+            enemy.transform.position = Vector3.MoveTowards(enemy.transform.position, enemyDest + playerOffset, moveSpeed * Time.deltaTime);
+        }
+
+        currentTime = 0;
+        playerChance = 50;
+        enemyChance = 50;
+        turns = 3;
+        StartCoroutine(Move());
     }
 
 }
+
+
--- H1ghN00n/Assets/Scripts/GridGenerator.cs
+++ H1ghN00n/Assets/Scripts/GridGenerator.cs
@@ -5,20 +5,52 @@ using UnityEngine;
 public class GridGenerator : MonoBehaviour
 {
     [SerializeField] GameObject hexPrefab;
+    public enum HexState { Empty, Player, Enemy, Cover, Pickup, Active }
+    public class Hex {
+        public Hex(GameObject tile, HexState state) {
+            this.tile = tile;
+            this.state = state;
+        }
+        public void SetTile(GameObject tile) {
+            this.tile = tile;
+        }
+        public void SetState(HexState state) {
+            this.state = state;
+        }
+        public HexState GetState() {
+            return state;
+        }
+        public GameObject GetTile() {
+            return tile;
+        }
+        private GameObject tile;
+        private HexState state;
+    }
+
+    public enum Direction { UL, UR, L, C, R, DL, DR };
+    public Dictionary<Direction, Vector2Int> HexDir = new Dictionary<Direction, Vector2Int>(){
+        { Direction.UL, new Vector2Int(-1,1) },
+        { Direction.UR, new Vector2Int(0,1) },
+        { Direction.L, new Vector2Int(-1,0) },
+        { Direction.C, new Vector2Int(0,0) },
+        { Direction.R, new Vector2Int(1,0) },
+        { Direction.DL, new Vector2Int(0,-1) },
+        { Direction.DR, new Vector2Int(1,-1) }
+    };
     //Dependant of hex prefab in world space
     float xOffset = 0.5f , yOffset = 0.85f;
-    public Dictionary<Vector2Int,GameObject> hexGrid; 
+    public Dictionary<Vector2Int,Hex> hexGrid; 
 
     public void GenerateGrid(Vector2Int dim) {
 
-        hexGrid = new Dictionary<Vector2Int,GameObject>();
+        hexGrid = new Dictionary<Vector2Int,Hex>();
 
         float xOffsetSum = 0;
         for (int i = 0; i < dim.y; i++) {
             for (int j = 0; j < dim.x; j++) {
                 GameObject hex = Instantiate(hexPrefab, new Vector3(j + xOffsetSum, i * yOffset, 1), Quaternion.identity, transform);
                 hex.name = $"[{j},{i}]";
-                hexGrid[new Vector2Int(j,i)] = hex;
+                hexGrid[new Vector2Int(j, i)] = new Hex(hex,HexState.Empty);
             }
             xOffsetSum += xOffset;
         }
@@ -28,10 +60,10 @@ public class GridGenerator : MonoBehaviour
                 //This if statement is a quick fix for index error when width is smaller than height/2 and it won't
                 //work if tiles are not named exactly how they're named. Either fix later or don't rename tiles
                 if (GameObject.Find($"[{i},{j}]") && GameObject.Find($"[{dim.x - 1 - i},{dim.y - 1 - j}]")) {
-                    GameObject tmp = hexGrid[new Vector2Int(i, j)];
+                    GameObject tmp = hexGrid[new Vector2Int(i, j)].GetTile();
                     hexGrid.Remove(GetCoord(tmp));
                     Destroy(tmp);
-                    tmp = hexGrid[new Vector2Int(dim.x - 1 - i, dim.y - 1 - j)];
+                    tmp = hexGrid[new Vector2Int(dim.x - 1 - i, dim.y - 1 - j)].GetTile();
                     hexGrid.Remove(GetCoord(tmp));
                     Destroy(tmp);
                 }
@@ -40,14 +72,47 @@ public class GridGenerator : MonoBehaviour
     }
 
     public Vector3 GetWorldPos(Vector2Int coord) {
-        return hexGrid[coord].transform.position;
+        return hexGrid[coord].GetTile().transform.position;
     }
 
     public Vector2Int GetCoord(GameObject hex) {
         //There will be only 1 value for each key so this is fine
-        foreach (KeyValuePair<Vector2Int,GameObject> myHex in hexGrid) {
-            if (myHex.Value == hex) return myHex.Key;
+        foreach (KeyValuePair<Vector2Int,Hex> myHex in hexGrid) {
+            if (myHex.Value.GetTile() == hex) return myHex.Key;
         }
         return new Vector2Int(-1,-1);
+    }
+
+    public int GetDistance(GameObject hex1, GameObject hex2) {
+        Vector2Int coord1 = GetCoord(hex1), coord2 = GetCoord(hex2);
+        int count = 0;
+        while(true) {
+            if(coord1.y==coord2.y) { 
+                if(coord1.x==coord2.x) {
+                    return count;
+                } else if (coord1.x<coord2.x) {
+                    coord1 += HexDir[Direction.R];
+                } else {
+                    coord1 += HexDir[Direction.L];
+                }
+            } else if (coord1.y<coord2.y) {
+                if (coord1.x == coord2.x) {
+                    coord1 += HexDir[Direction.UR];
+                } else if (coord1.x < coord2.x) {
+                    coord1 += HexDir[Direction.R];
+                } else {
+                    coord1 += HexDir[Direction.UL];
+                }
+            } else {
+                if (coord1.x == coord2.x) {
+                    coord1 += HexDir[Direction.DL];
+                } else if (coord1.x < coord2.x) {
+                    coord1 += HexDir[Direction.DR];
+                } else {
+                    coord1 += HexDir[Direction.L];
+                }
+            }
+            count += 1;
+        }
     }
 }
